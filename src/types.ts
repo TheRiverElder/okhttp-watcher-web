80bb6bae91792b7f0653437403e84080b45ad05c
TheRiverElder <765375399@qq.com>
2024-06-14 22:05:43 +0800

    修改根路径为相对路径

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,11 @@ export interface OkHttpState {
     uid: number;
     request: {
         url: string;
+        // headers: object;
+        // body?: Uint8Array;
     };
     response: {
+        // headers: object;
         body: {
             charset: string;
             content: Uint8Array;
